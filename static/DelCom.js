8c6e3e1b88dd8d08a29c994182cc7cef2815964f
mpurtle <mpurtle@uark.edu>
2023-09-13 14:07:28 -0500

    Finished?

--- static/DelCom.js
+++ static/DelCom.js
@@ -1,13 +1,36 @@
 async function markComplete(Complete){
-    const id = Complete.getAttribute('id')
+    const id = Complete.getAttribute('data-id')
     
     let para = document.querySelector(`[data-id="${id}"]`)
     para.innerHTML = "Completed"
  
 
-    await fetch(`http://127.0.0.1:8000/complete/${id}`)
+    await fetch(`http://127.0.0.1:8000/complete/${id}`, {
+        'method': 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+          },
+    })
 }
 
-function markDelete(){
-    console.log("No")
+async function markDelete(Delete){
+    const id = Delete.getAttribute('data-id')
+    
+    let para = document.querySelector(`[data-id="${id}"]`)
+    para.innerHTML = "Deleted"
+ 
+
+    await fetch(`http://127.0.0.1:8000/delete/${id}`, {
+        'method': 'delete',
+        headers: {
+            'Content-type': 'application/json'
+          },
+    })
+    .then(() => {
+        const container = document.getElementById("main")
+        let nada = document.getElementById(`${id}`) 
+
+        container.removeChild(nada)
+    }
+    )
 }
--- static/POSTTask.js
+++ static/POSTTask.js
@@ -31,17 +31,48 @@ document.addEventListener("DOMContentLoaded", function () {
                 col.classList.add("col-12", "d-flex", "justify-content-center");
                 inputGroup.classList.add("input-group", "d-flex", "justify-content-center");
                 para.classList.add("lead", "text-justify", "m-sm");
+                para.setAttribute("data-id", yes.task_id)
                 para.innerText = yes.String_Todo;
+
                 butCom.classList.add("btn", "btn-outline-secondary");
                 butCom.setAttribute('id', yes.task_id)
-                butCom.addEventListener("click", () => {
-                    console.log("Hello")
+                butCom.addEventListener("click",async () => {
+                    const id = yes.task_id
+
+                    let para = document.querySelector(`[data-id="${id}"]`)
+                    para.innerHTML = "Completed"
+ 
+
+                    await fetch(`http://127.0.0.1:8000/complete/${id}`, {
+                        'method': 'PUT',
+                        headers: {
+                            'Content-type': 'application/json'
+                         },
+                    })
                 })
                 butCom.type = "button";
                 butCom.innerText = "Complete";
                 butDel.classList.add("btn", "btn-outline-secondary");
-                butDel.addEventListener("click", () => {
-                    console.log("Hello!")
+                butDel.addEventListener("click", async () => {
+                    const id = yes.task_id
+    
+                    let para = document.querySelector(`[data-id="${id}"]`)
+                    para.innerHTML = "Deleted"
+ 
+
+                    await fetch(`http://127.0.0.1:8000/delete/${id}`, {
+                    'method': 'delete',
+                    headers: {
+                        'Content-type': 'application/json'
+                        },
+                    })
+                    .then(() => {
+                    const container = document.getElementById("main")
+                    let nada = document.getElementById(`${id}`) 
+
+                    container.removeChild(nada)
+                    }
+                )
                 })
                 butDel.setAttribute('id', yes.task_id)
                 butDel.type = "button";
